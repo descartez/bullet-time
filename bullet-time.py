67e0a9f769c97a7cc219475218887457eef19850
Ryan Stong <rvanstong@gmail.com>
2017-06-21 07:57:19 -0700

    adds in clock animation, feels right now, i think its done

--- bullet-time.py
+++ bullet-time.py
@@ -27,17 +27,38 @@ class GunGame():
                         "35900:"
                         "00000:"
                         "00500:")
+        a_fire4 = Image("00000:"
+                        "00000:"
+                        "35669:"
+                        "00000:"
+                        "00000:")
 
-        gunfire_a = [a_fire1, a_fire2, a_fire3]
-        gunfire_b = []
+        b_fire1 = Image("00000:"
+                        "00000:"
+                        "00059:"
+                        "00000:"
+                        "00000:")
+        b_fire2 = Image("00000:"
+                        "00050:"
+                        "00095:"
+                        "00050:"
+                        "00000:")
+        b_fire3 = Image("00500:"
+                        "00000:"
+                        "00953:"
+                        "00000:"
+                        "00500:")
+        b_fire4 = Image("00000:"
+                        "00000:"
+                        "99663:"
+                        "00000:"
+                        "00000:")
+
+        gunfire_a = [a_fire1, a_fire2, a_fire3, a_fire4]
+        gunfire_b = [b_fire1, b_fire2, b_fire3, b_fire4]
 
         display.clear()
-        display.show("3")
-        sleep(1000)
-        display.show("2")
-        sleep(random.randint(500,2000))
-        display.show("1")
-        sleep(200)
+        display.show(Image.ALL_CLOCKS, loop=False, delay=random.randint(10,200))
         display.show(Image.SQUARE)
 
         playing = True
@@ -45,14 +66,15 @@ class GunGame():
         while(playing):
             if button_a.is_pressed():
                 winner = "A"
-                display.show(gunfire_a, delay=100)
+                display.show(gunfire_a, delay=30)
+                display.clear()
                 sleep(1000)
                 playing = False
 
             if button_b.is_pressed():
                 winner = "B"
-                display.show(Image.ARROW_E)
-                sleep(1000)
+                display.show(gunfire_b, delay=30)
+                display.clear()
                 playing = False
 
         display.scroll(winner)
@@ -62,12 +84,16 @@ class GunGame():
         sleep(500)
         display.clear()
 
-
-
-
 game = GunGame()
 
 while True:
+    both_arrows = Image("00000:"
+                        "09090:"
+                        "99999:"
+                        "09090:"
+                        "00000:")
+    display.show(both_arrows)
+
     if button_a.is_pressed() and button_b.is_pressed():
         game.startGame()
 
